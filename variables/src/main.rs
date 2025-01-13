151ae27ab139cec49918e48663ac3458cd5ce130
Hong <hong@ubuntu-lab-01>
2025-01-13 21:54:43 +0800

    Update codes

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,6 +1,26 @@
+const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
+const TWO: u32 = 1 + 1;
 fn main() {
-    let x = 5;
+    //Variables and Mutability
+    let mut x = 5;
     println!("The value of x is: {x}");
     x = 6;
     println!("The value of x is: {x}");
+    //Constants
+    println!("THREE_HOURS_IN_SECONDS:{THREE_HOURS_IN_SECONDS}");
+    println!("TWO:{TWO}");
+    //Shadowing
+    let y = 60;
+    let y = y + 9;
+    {
+        let y = y * 2;
+        println!("The value of y in the innner scope is: {y}");
+    }
+    println!("The value of y is: {y}");
+    let spaces = "   ";
+    let spaces = spaces.len();
+    println!("string len:{spaces}");
+    // let mut spaces = "   ";
+    // spaces = spaces.len();
+    // println!("string len:{spaces}");
 }
