03e9abc65c4ec37d77ea85027eae2853a694d450
Hong <hong@ubuntu-lab-01>
2025-02-23 00:16:49 +0800

    Update ownership/src/main.rs

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,3 +1,5 @@
+// Safety is the Absence of Undefined Behavior
+
 // fn read(y: bool) {
 //     if y {
 //         println!("y is true!");
@@ -9,13 +11,41 @@
 //     read(x);
 // }
 
-fn read(y: bool) {
-    if y {
-        println!("y is true!");
-    }
-}
+// fn read(y: bool) {
+//     if y {
+//         println!("y is true!");
+//     }
+// }
+
+// fn main() {
+//     read(x); // oh no! x isn't defined!
+//     let x = true;    
+// }
+
+// Variables Live in the Stack
+// fn main() {
+//     let n = 5;
+//     let y = plus_one(n);
+//     println!("The value of y is: {y}");
+// }
+
+// fn plus_one(x: i32) -> i32 {
+//     x + 1
+// }
+
+// fn main() {
+// let a = 5;
+// let mut b = a;
+// b += 1;
+// }
+
+// Boxes Live in the Heap
+// fn main() {
+// let a = [0; 1_000_000];
+// let b = a;
+// }
 
 fn main() {
-    read(x); // oh no! x isn't defined!
-    let x = true;
+let a = Box::new([0; 1_000_000]);
+let b = a;
 }
