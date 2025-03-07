af41e232dd7fdbaeb0f7e3ff39fdfdc641c21613
Hong <hong@ubuntu-lab-01>
2025-03-08 04:00:32 +0800

    Up to 4.2. References and Borrowing

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -133,25 +133,101 @@
 // Question 4
 
 
-fn move_a_box(b: Box<i32>) {
-  // This space intentionally left blank              
-}
+// fn move_a_box(b: Box<i32>) {
+//   // This space intentionally left blank              
+// }
 
-fn main() {
-    // let b = Box::new(0);
-    // move_a_box(b);
-    // println!("{}", b);
+// fn main() {
+//     // let b = Box::new(0);
+//     // move_a_box(b);
+//     // println!("{}", b);
 
-    // let b = Box::new(0);
-    // let b2 = b;
-    // move_a_box(b);
+//     // let b = Box::new(0);
+//     // let b2 = b;
+//     // move_a_box(b);
 
-    let b = Box::new(0);
-    let b2 = b;
-    println!("{}", b);
-    move_a_box(b2);
+//     let b = Box::new(0);
+//     let b2 = b;
+//     println!("{}", b);
+//     move_a_box(b2);
 
-    // let b = Box::new(0);
-    // move_a_box(b);
-    // let b2 = b;
+//     // let b = Box::new(0);
+//     // move_a_box(b);
+//     // let b2 = b;
+// }
+
+// Rust data structures
+// Box
+// Vec
+// String
+// HashMap
+
+// References and Borrowing
+
+// fn main() {
+//     let m1 = String::from("Hello");
+//     let m2 = String::from("world");
+//     greet(m1, m2);
+//     let s = format!("{} {}", m1, m2); // Error: m1 and m2 are moved
+// }
+
+// fn greet(g1: String, g2: String) {
+//     println!("{} {}!", g1, g2);
+// }
+
+// fn main() {
+//     let m1 = String::from("Hello");
+//     let m2 = String::from("world");
+//     let (m1_again, m2_again) = greet(m1, m2);
+//     let s = format!("{} {}", m1_again, m2_again);
+// }
+
+// fn greet(g1: String, g2: String) -> (String, String) {
+//     println!("{} {}!", g1, g2);
+//     (g1, g2)
+// }
+
+// References Are Non-Owning Pointers
+
+// fn main() {
+//     let m1 = String::from("Hello");
+//     let m2 = String::from("world");
+//     greet(&m1, &m2); // note the ampersands
+//     let s = format!("{} {}", m1, m2);
+// }
+
+// fn greet(g1: &String, g2: &String) { // note the ampersands
+//     println!("{} {}!", g1, g2);
+// }
+
+// Dereferencing a Pointer Accesses Its Data
+
+// fn main() {
+// let mut x: Box<i32> = Box::new(1);
+// let a: i32 = *x;         // *x reads the heap value, so a = 1
+// *x += 1;                 // *x on the left-side modifies the heap value,
+//                          //     so x points to the value 2
+
+// let r1: &Box<i32> = &x;  // r1 points to x on the stack
+// let b: i32 = **r1;       // two dereferences get us to the heap value
+
+// let r2: &i32 = &*x;      // r2 points to the heap value directly
+// let c: i32 = *r2;    // so only one dereference is needed to read it
+// }
+
+fn main()  {
+let x: Box<i32> = Box::new(-1);
+let x_abs1 = i32::abs(*x); // explicit dereference
+let x_abs2 = x.abs();      // implicit dereference
+assert_eq!(x_abs1, x_abs2);
+
+let r: &Box<i32> = &x;
+let r_abs1 = i32::abs(**r); // explicit dereference (twice)
+let r_abs2 = r.abs();       // implicit dereference (twice)
+assert_eq!(r_abs1, r_abs2);
+
+let s = String::from("Hello");
+let s_len1 = str::len(&s); // explicit reference
+let s_len2 = s.len();      // implicit reference
+assert_eq!(s_len1, s_len2);
 }
