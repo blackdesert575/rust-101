b414cf05d1fc0c75998f444282e95854670add55
hong539 <yunghunghuang984@gmail.com>
2025-08-31 00:24:04 +0800

    Update BRFNTP/ch02/**, Add BRFNTP/ch03/**

--- BRFNTP/ch02/src/main.rs
+++ BRFNTP/ch02/src/main.rs
@@ -7,5 +7,25 @@ fn main() {
     // print!("{}", (23. - 6.) % 5. + 20. * 30. / (3. + 4.));
     // print!("{}", 2.7 + 1);
     // print!("{}", 2.7 + 1.);
-    print!("{} {}", -12 % 10, -1.2 % 1.);
+    // print!("{} {}", -12 % 10, -1.2 % 1.);
+    // print!("{} + ", 80);
+    // print!("{} =", 34);
+    // print!(" {}", 80 + 34);
+    // print!("{} + ",80);print!("{} = ", 34);
+    //         print  ! ( "{}"  ,            
+    //     80     + 34 )  ;
+    // println!("{}", "This"
+    //     "is not allowed");
+    // println!("{}", "These
+    //     are
+    //     three lines");
+    // println!("{}", "This \
+    //     is \
+    //     just one line");
+//     println!("{}", "These
+// are
+// three lines");
+    println!("{}", "These\n\
+        are\n\
+        three lines");
 }
---
+++ BRFNTP/ch03/src/main.rs
@@ -0,0 +1,5 @@
+fn main() {
+    let number = 12;
+    let other_number = 53;
+    print!("{}\n", number + other_number);
+}
