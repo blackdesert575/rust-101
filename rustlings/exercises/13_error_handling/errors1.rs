dc6c8e797f6c1cc3b03f674281616dd0fc3a22ec
hong539 <yunghunghuang984@gmail.com>
2025-11-09 10:48:21 +0800

    modified:   rustlings/.rustlings-state.txt         modified:   rustlings/exercises/13_error_handling/errors1.rs         modified:   rustlings/solutions/13_error_handling/errors1.rs

--- rustlings/exercises/13_error_handling/errors1.rs
+++ rustlings/exercises/13_error_handling/errors1.rs
@@ -4,17 +4,60 @@
 // construct to `Option` that can be used to express error conditions. Change
 // the function signature and body to return `Result<String, String>` instead
 // of `Option<String>`.
-fn generate_nametag_text(name: String) -> Option<String> {
+fn generate_nametag_text(name: String) -> Result<String, String> {
     if name.is_empty() {
         // Empty names aren't allowed
-        None
+        Err("Empty names aren't allowed".to_string())
     } else {
-        Some(format!("Hi! My name is {name}"))
+        Ok(format!("Hi! My name is {name}"))
     }
 }
 
 fn main() {
     // You can optionally experiment here.
+    // just print to stdout
+    println!("case: 1");
+    let input = String::from("");
+    println!("{:#?}", generate_nametag_text(input));
+    // with unwrap()
+    println!("--------------------------------------");
+    println!("case: 2");
+    let ok_input = String::from("David");
+    let result = generate_nametag_text(ok_input);
+    println!("{:#?}", result);
+
+    let err_input = String::new();
+    let result = generate_nametag_text(err_input);
+    print!("{:#?}", result);
+    // with .expect("msg")
+    println!("--------------------------------------");
+    println!("case: 3");
+    println!("remove comments to show case 3");
+    // let ok_input = String::from("Asuka");
+    // let result = generate_nametag_text(ok_input).expect("Name tag generation failed!");
+    // println!("{:#?}", result);
+
+    // let err_input = String::new();
+    // let result = generate_nametag_text(err_input).expect("Expected a valid name, but got empty string!");
+    // print!("{:#?}", result);
+    // with match
+    println!("--------------------------------------");
+    println!("case: 4");
+    let input = String::from("");
+    match generate_nametag_text(input) {
+        Ok(text) => println!("Generated tag: {text}"),
+        Err(err) => println!("Failed: {err}"),
+        
+    }
+    // with if let
+    println!("--------------------------------------");
+    println!("case: 5");
+    let input = String::from("Rico");
+    if let Ok(..) = generate_nametag_text(input) {
+        println!("OK");
+    } else {
+        print!("Failed")
+    }
 }
 
 #[cfg(test)]
