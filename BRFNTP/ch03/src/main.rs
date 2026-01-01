598c8a8280eac51fb8bd5708cc65053b57324c72
hong539 <yunghunghuang984@gmail.com>
2026-01-01 22:25:45 +0800

    BRFNTP (Beginning Rust From Novice to Professional) ch03: Mutable Variables

--- BRFNTP/ch03/src/main.rs
+++ BRFNTP/ch03/src/main.rs
@@ -1,5 +1,24 @@
 fn main() {
+    // let number = 12;
+    // let other_number = 53;
+    // print!("{}\n", number + other_number);
+    // let number = 12;
+    // print!("{}", number);
+    // let number = 41;
+    // print!("{} {}", number, 47);
+    //case01
     let number = 12;
-    let other_number = 53;
-    print!("{}\n", number + other_number);
+    print!("{}", number);
+    let number = 53;
+    print!(" {}", number);
+    //case02    
+    // let number = 12;
+    // print!("{}", number);
+    // number = 53;
+    // print!(" {}", number);
+    //case03
+    // let mut number = 12;
+    // print!("{}", number);
+    // number = 53;
+    // print!(" {}", number);    
 }
